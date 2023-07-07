108691f0f8f3f785a0eae9338bc7975c18ece402
Aleksandra Emelina <alexa.emelina>
2023-07-07 05:17:19 +0400

    listen on port 3000

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
+import dns from 'node:dns'
 import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/server-options.html#server-host
+dns.setDefaultResultOrder('verbatim');
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    open: true,
+    port: 3000,
+    strictPort: true,
+  },
   plugins: [react()],
 })
